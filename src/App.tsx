b791ccf1d18b292a2fa923d8f9c37dbd9ae5c153
jurasw <juraswis@gmail.com>
2023-05-22 22:45:06 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import "./styles.css";
 const App: React.FC = () => {
-  const theme = extendTheme({
-    components: {
-      Button: {
-        _focus: {
-          outline: "none",
-        },
-      },
-    },
-  });
   return (
     <BrowserRouter>
-      <ChakraProvider theme={theme}>
+      <ChakraProvider>
         <Routes>
           <Route>
             <Route index element={<Home />} />
