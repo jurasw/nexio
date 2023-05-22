8fb9ea577636290c594dedb98f4806e29cdbcddb
jurasw <juraswis@gmail.com>
2023-05-22 11:00:45 +0200

    div

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,15 +7,16 @@ import MyComponent from "../components/home/UnderHero";
 import Technologies from "../components/home/Technologies";
 import CaseStudy from "../components/home/CaseStudy";
 import TempTestimonials from "../components/home/TempTestimonials";
-import { Container } from "@chakra-ui/react";
 
 const Home: React.FC = () => {
   return (
-    <Container
-    backgroundImage= "url('/hero.png')"
-    backgroundSize= {["cover", "contain"]}
-    fontFamily= "Red Hat Display"
-    backgroundRepeat= "no-repeat"
+    <div
+      style={{
+        backgroundImage: "url('/hero.png')",
+        backgroundSize: "contain",
+        fontFamily: "Red Hat Display",
+        backgroundRepeat: "no-repeat",
+      }}
     >
       <Nav />
       <div>
@@ -27,7 +28,7 @@ const Home: React.FC = () => {
         <TempTestimonials />
       </div>
       <Footer />
-    </Container>
+    </div>
   );
 };
 
