b32a8c369bccc58047f01a76103e1992c47c5631
jurasw <juraswis@gmail.com>
2023-05-23 11:46:23 +0200

    images

--- src/components/home/UnderHero.tsx
+++ src/components/home/UnderHero.tsx
@@ -78,7 +78,7 @@ const MyComponent: React.FC = () => {
               <Flex w="100%" h={["0em", "8em"]} mt="20px" />
               <Card
                 imageUrl={
-                  "https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                  "mlody-przystojny-mezczyzna-praca-w-biurze-przy-uzyciu-komputera.jpg"
                 }
                 title={"Dla biznesu"}
                 text={
@@ -112,7 +112,7 @@ const MyComponent: React.FC = () => {
             >
               <Card
                 imageUrl={
-                  "https://images.unsplash.com/photo-1515378791036-0648a3ef77b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+                  "powazni-koledzy-w-biurze-rozmawiaja-ze-soba.jpg"
                 }
                 title={"Kariera"}
                 text={
