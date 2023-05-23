426d417f06cd8b361a5ae65e2df2af854881e7dc
jurasw <juraswis@gmail.com>
2023-05-23 10:29:06 +0200

    nasza oferta btn to modal

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -9,105 +9,113 @@ import {
   Flex,
   Image,
   Link,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
 } from "@chakra-ui/react";
 import { motion } from "framer-motion";
+import { useState } from "react";
+import BusinessModalBodyContent from "../business/ModalBodyContent";
 
 const Hero = () => {
+  const [business, setBusiness] = useState(false);
+
   return (
-    <Container
-      maxW={"100%"}
-      px={"10%"}
-      py={"5%"}
-      minH="100vh"
-      bg={["rgba(255, 208, 151, 0.1)", "rgba(255, 208, 151, 0)"]}
-      mt={0}
-    >
-      <Stack
-        align={"center"}
-        spacing={{ base: 8, md: 10 }}
-        py={{ base: 10, md: 14 }}
-        direction={{ base: "column", md: "row" }}
+    <>
+      <Container
+        maxW={"100%"}
+        px={"10%"}
+        py={"5%"}
+        minH="100vh"
+        bg={["rgba(255, 208, 151, 0.1)", "rgba(255, 208, 151, 0)"]}
+        mt={0}
       >
-        <Stack flex={1} spacing={{ base: 5, md: 10 }}>
-          <Heading
-            lineHeight={1.1}
-            fontWeight={600}
-            // fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
-          >
-            <Text
-              fontSize={["50px", "70px"]}
-              as={"span"}
-              position={"relative"}
-              bgGradient="linear(to-l,#F19E38, #000000)"
-              bgClip="text"
-              _after={{
-                content: "''",
-                width: "full",
-                height: "30%",
-                position: "absolute",
-                bottom: 1,
-                left: 0,
-                zIndex: -1,
-              }}
+        <Stack
+          align={"center"}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 10, md: 14 }}
+          direction={{ base: "column", md: "row" }}
+        >
+          <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+            <Heading
+              lineHeight={1.1}
+              fontWeight={600}
+              // fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
             >
-              Digital future
-            </Text>
-            <br />
-            <Text fontSize={["50px", "70px"]} as={"span"} color={"#00"}>
-              encoded with trust.
-            </Text>
-          </Heading>
-          <Text color={"#000"}>
-            Nexio Management to firma technologiczno-konsultingowa, świadcząca{" "}
-            <br />
-            kompleksowe usługi w obszarze budowy, utrzymania i rozwoju rozwiązań{" "}
-            <br /> softwareowych.
-          </Text>
-          <Stack
-            spacing={{ base: 4, sm: 6 }}
-            direction={["column", "column", "row"]}
-            textAlign={"center"}
-          >
-            <Link href={"#footer"}>
-              <Button
-                maxW={"235px"}
-                // m={["auto", "auto", 0]}
-                rounded={"full"}
-                fontSize={"16px"}
-                fontWeight={"normal"}
-                px={6}
-                py={4}
-                border="2px"
-                borderColor={"#F19E38"}
-                bg={"rgba(255, 255, 255, 0)"}
-                _hover={{
-                  borderColor: "#F19E38",
-                  bg: "rgba(255, 208, 151, 0.2)",
-                  color: "#000",
-                }}
-                _active={{
-                  borderColor: "#F19E38",
-                  bg: "rgba(255, 208, 151, 0.2)",
+              <Text
+                fontSize={["50px", "70px"]}
+                as={"span"}
+                position={"relative"}
+                bgGradient="linear(to-l,#F19E38, #000000)"
+                bgClip="text"
+                _after={{
+                  content: "''",
+                  width: "full",
+                  height: "30%",
+                  position: "absolute",
+                  bottom: 1,
+                  left: 0,
+                  zIndex: -1,
                 }}
               >
-                Porozmawiaj z ekspertem
-              </Button>
-            </Link>
-            <div
-              style={{
-                justifyContent: "center",
-                textAlign: "center",
-                display: "flex",
-                alignItems: "center",
-              }}
+                Digital future
+              </Text>
+              <br />
+              <Text fontSize={["50px", "70px"]} as={"span"} color={"#00"}>
+                encoded with trust.
+              </Text>
+            </Heading>
+            <Text color={"#000"}>
+              Nexio Management to firma technologiczno-konsultingowa, świadcząca{" "}
+              <br />
+              kompleksowe usługi w obszarze budowy, utrzymania i rozwoju
+              rozwiązań <br /> softwareowych.
+            </Text>
+            <Stack
+              spacing={{ base: 4, sm: 6 }}
+              direction={["column", "column", "row"]}
+              textAlign={"center"}
             >
-              <Link
-                _hover={{
-                  textDecoration: "none",
+              <Link href={"#footer"}>
+                <Button
+                  maxW={"235px"}
+                  // m={["auto", "auto", 0]}
+                  rounded={"full"}
+                  fontSize={"16px"}
+                  fontWeight={"normal"}
+                  px={6}
+                  py={4}
+                  border="2px"
+                  borderColor={"#F19E38"}
+                  bg={"rgba(255, 255, 255, 0)"}
+                  _hover={{
+                    borderColor: "#F19E38",
+                    bg: "rgba(255, 208, 151, 0.2)",
+                    color: "#000",
+                  }}
+                  _active={{
+                    borderColor: "#F19E38",
+                    bg: "rgba(255, 208, 151, 0.2)",
+                  }}
+                >
+                  Porozmawiaj z ekspertem
+                </Button>
+              </Link>
+              <div
+                style={{
+                  justifyContent: "center",
+                  textAlign: "center",
+                  display: "flex",
+                  alignItems: "center",
                 }}
-                href="https://www.pracuj.pl/praca/nexio%20management%20sp.%20z%20o.o.;en"
               >
                 <Button
+                  onClick={() => {
+                    setBusiness(!business);
+                  }}
                   rounded={"full"}
                   display="flex"
                   size={"lg"}
@@ -128,111 +136,130 @@ const Hero = () => {
                     src={"arrow-right-circle.svg"}
                   />
                 </Button>
-              </Link>
-            </div>
+              </div>
+            </Stack>
           </Stack>
         </Stack>
-      </Stack>
-      <Flex
-        direction={["column", "column", "row"]}
-        marginTop={["10px", "100px"]}
-        w="100%"
-      >
-        <Flex direction="column" w={["100%", "100%", "60%"]} maxW={"100%"}>
-          <Text color="#A1A1A1" fontWeight={600} fontSize="10px">
-            PARTNERS & CLIENTS
-          </Text>
-          <div
-            style={{
-              display: "flex",
-              flexWrap: "wrap",
-              maxWidth: "100%",
-              marginTop: "20px",
-            }}
-          >
-            <Image
-              src={"icons/companies/lot.svg"}
-              style={{ marginBottom: "10px" }}
-            />
-            <Image
-              marginLeft="5%"
-              src={"icons/companies/orange.svg"}
-              style={{ marginBottom: "10px" }}
-            />
-            <Image
-              marginLeft="5%"
-              src={"icons/companies/tmobile.svg"}
-              style={{ marginBottom: "10px" }}
-            />
-            <Image
-              marginLeft="5%"
-              src={"icons/companies/netia.svg"}
-              style={{ marginBottom: "10px" }}
-            />
-            <Image
-              marginLeft="5%"
-              src={"icons/companies/tvn.svg"}
-              style={{ marginBottom: "10px" }}
-            />
-            <Image marginLeft="5%" src={"icons/companies/fedex.svg"} />
-          </div>
-        </Flex>
-
-        <Flex direction="column" w={["100%", "100%", "40%"]} display="flex">
-          <div style={{ marginLeft: "auto", display: "flex" }}>
-            <Text
-              color="#000"
-              fontWeight={700}
-              fontSize="12px"
-              alignItems="center"
-              justifyContent="center"
-              display="flex"
-              marginRight={"20px"}
-            >
-              SCROLL DOWN
+        <Flex
+          direction={["column", "column", "row"]}
+          marginTop={["10px", "100px"]}
+          w="100%"
+        >
+          <Flex direction="column" w={["100%", "100%", "60%"]} maxW={"100%"}>
+            <Text color="#A1A1A1" fontWeight={600} fontSize="10px">
+              PARTNERS & CLIENTS
             </Text>
             <div
               style={{
-                background: "#fff",
-                width: "60px",
-                height: "60px",
-                border: "1px solid #FFD097",
-                borderRadius: "100%",
-
-                alignItems: "center",
-                justifyContent: "center",
                 display: "flex",
+                flexWrap: "wrap",
+                maxWidth: "100%",
+                marginTop: "20px",
               }}
             >
-              <Container
-                bg="#fff"
-                w="46px"
-                h="46px"
-                borderRadius={"100%"}
-                borderWidth={1}
-                borderColor="#E9E9E9"
-                margin="auto"
-              >
-                <motion.div
-                  animate={{ y: [-10, 10] }}
-                  transition={{
-                    repeat: Infinity,
-                    duration: 1,
-                    repeatType: "reverse",
-                  }}
-                >
-                  <Image
-                    position="relative"
-                    top={"-20px"}
-                    src={"icons/scroll-arrow.svg"}
-                  />
-                </motion.div>
-              </Container>
+              <Image
+                src={"icons/companies/lot.svg"}
+                style={{ marginBottom: "10px" }}
+              />
+              <Image
+                marginLeft="5%"
+                src={"icons/companies/orange.svg"}
+                style={{ marginBottom: "10px" }}
+              />
+              <Image
+                marginLeft="5%"
+                src={"icons/companies/tmobile.svg"}
+                style={{ marginBottom: "10px" }}
+              />
+              <Image
+                marginLeft="5%"
+                src={"icons/companies/netia.svg"}
+                style={{ marginBottom: "10px" }}
+              />
+              <Image
+                marginLeft="5%"
+                src={"icons/companies/tvn.svg"}
+                style={{ marginBottom: "10px" }}
+              />
+              <Image marginLeft="5%" src={"icons/companies/fedex.svg"} />
             </div>
-          </div>
+          </Flex>
+
+          <Flex direction="column" w={["100%", "100%", "40%"]} display="flex">
+            <div style={{ marginLeft: "auto", display: "flex" }}>
+              <Text
+                color="#000"
+                fontWeight={700}
+                fontSize="12px"
+                alignItems="center"
+                justifyContent="center"
+                display="flex"
+                marginRight={"20px"}
+              >
+                SCROLL DOWN
+              </Text>
+              <div
+                style={{
+                  background: "#fff",
+                  width: "60px",
+                  height: "60px",
+                  border: "1px solid #FFD097",
+                  borderRadius: "100%",
+
+                  alignItems: "center",
+                  justifyContent: "center",
+                  display: "flex",
+                }}
+              >
+                <Container
+                  bg="#fff"
+                  w="46px"
+                  h="46px"
+                  borderRadius={"100%"}
+                  borderWidth={1}
+                  borderColor="#E9E9E9"
+                  margin="auto"
+                >
+                  <motion.div
+                    animate={{ y: [-10, 10] }}
+                    transition={{
+                      repeat: Infinity,
+                      duration: 1,
+                      repeatType: "reverse",
+                    }}
+                  >
+                    <Image
+                      position="relative"
+                      top={"-20px"}
+                      src={"icons/scroll-arrow.svg"}
+                    />
+                  </motion.div>
+                </Container>
+              </div>
+            </div>
+          </Flex>
         </Flex>
-      </Flex>
-    </Container>
+      </Container>
+      <Modal
+        id="biznes"
+        isOpen={business}
+        onClose={() => {
+          setBusiness(!business);
+        }}
+      >
+        <ModalOverlay />
+        <ModalContent maxW="1200px" borderRadius="32px" paddingBottom={"120px"}>
+          <ModalHeader fontSize="38px" paddingTop="50px">
+            Zadbaj o rozwój swojego biznesu z zaufanym partnerem
+            technologicznym.
+          </ModalHeader>
+          <ModalCloseButton margin={["0", "50px"]} />
+          <ModalBody>
+            <BusinessModalBodyContent />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
